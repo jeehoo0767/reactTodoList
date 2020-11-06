ecf57062154af0df7e9927279ac0970affe18d5f
alsruddl0805 <66991772+jeehoo0767@users.noreply.github.com>
2020-11-06 18:09:09 +0900

    Create React TodoList Component Form and Method

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
+
+class App extends React.Component{
+  id = 3;
+  constructor(props){
+    super(props);
+    this.stste = {
+      input : '',
+      todos : [
+        {id : 0, text : '리액트 소개', checked : false },
+        {id : 1, text : '리액트 소개', checked : true },
+        {id : 2, text : '리액트 소개', checked : false },
+      ]
+    }
+  }
+
+  handleChange = (e) =>{
+    this.setState({
+      input : e.target.value
+    })
+  };
+
+  handleCreate = () =>{
+    const { input, todos } = this.state;
+    this.setState({
+      input : '',
+      todos : todos.concat({
+        id : this.id++,
+        text : input,
+        checked : false
+      })
+    });
+  };
+
+  handleKeyPress = () =>{
+    if(e.key === 'Enter'){
+      this.handleCreate();
+    }
+  }
+
+  render(){
+    const { input } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress
+    } = this;
+
+    return (
+    <TodoListTemplate form = {<Form
+    value = {this.state.input} onKeyPress = {this.handleKeyPress}
+    onChange = {this.handleChange} onCreate = {this.handleCreate}  
+    />} 
+    children = {<TodoItemList/>}/>
+    );
+  };
+}
+
+export default App;
